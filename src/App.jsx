bc1c289097071f87c4a7881764da0711b37a4a89
ian otieno <ianotieno23@gmail.com>
2026-01-21 09:50:45 +0300

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>home</h1>,
+  },
+  {
+    path: '/about',
+    element: <div>About Page</div>
+  },
+]);
 function App() {
   return (
-    <h1>
-      Jobify Appp
-    </h1>
+    <RouterProvider router={router} />
   )
 }
 
