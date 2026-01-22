83237f025bb9e824d3991bb695e5227a4c0b6305
ian otieno <ianotieno23@gmail.com>
2026-01-22 15:06:08 +0300

    update react icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,13 @@ import Wrapper from '../assets/wrappers/NavBar'
 
 import { FaAlignLeft } from 'react-icons/fa';
 import Logo from './Logo';
-
+import { useDashboardContext } from '../pages/DashboardLayout';
 function NavBar() {
+    const { toggleSidebar } = useDashboardContext();
   return (
     <Wrapper>
      <div className='nav-center'>
-        <button type='button' className='toggle-btn' >
+        <button type='button' className='toggle-btn' onClick={toggleSidebar}>
           <FaAlignLeft />
         </button>
         <div>
