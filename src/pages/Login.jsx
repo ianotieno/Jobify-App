72c0d0fefb39e8b3052566cd485153d13e01685e
ian otieno <ianotieno23@gmail.com>
2026-01-22 11:26:29 +0300

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow ,Logo  } from '../components';
+import { Link } from 'react-router-dom';
 
 function   Login() {
   return (
-    <h1>  Login</h1>
+    <Wrapper>
+       <form className='form'>
+       <Logo />
+      <h4>Login </h4>
+      <FormRow type='email' name='email' defaultValue={'paul@gmail.com'} />
+      <FormRow type='password' name='password' defaultValue={'Paul123'} />
+      <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+         Not a member?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+       </form>
+      
+    </Wrapper>
   )
 }
 
